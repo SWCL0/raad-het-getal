8bb13bce5a809f7e2b45c585a034f676c52cafc0
SWCL0 <stan.w.c@icloud.com>
2024-02-09 09:49:32 +0100

    update raad-het-getal.php
    toevoegingen
    - getal generator
    -opvraag getal gebruiker
    - loop die aangeeft hoger of lager of correct

--- raad-het-getal.php
+++ raad-het-getal.php
@@ -1,5 +1,24 @@
 <?php
 
+$raadGetal = rand(1, 100);
+
+echo "Geef een getal :)" . PHP_EOL;
+
+
+
+while (true) {
+    $invoer = readline();
+
+    if ($invoer < $raadGetal) {
+        echo "Hoger!" . PHP_EOL;
+    } elseif ($invoer > $raadGetal) {
+        echo "Lager!" . PHP_EOL;
+    } else {
+        echo "Gefeliciteerd! Je hebt het getal goed geraden! Het getal was: $raadGetal";
+        break;
+    }
+}
+
 
 
 ?>
