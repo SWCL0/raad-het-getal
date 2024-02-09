9ce8c3b5cf955e3ac4014eb0b1ccb86b6599c21e
SWCL0 <stan.w.c@icloud.com>
2024-02-09 09:57:30 +0100

    update raad-het-getal.php
    toevoeging checks:
    - getal onder 1
    - getal boven 100

--- raad-het-getal.php
+++ raad-het-getal.php
@@ -5,12 +5,16 @@ $gokken = 0;
 
 echo "Geef een getal :)" . PHP_EOL;
 
-
-
 while (true) {
     $invoer = readline();
 
-    if ($invoer < $raadGetal) {
+    if ($invoer < 1) {
+        echo "Getal tussen de 1 - 100!";
+        exit;
+    } else if ($invoer > 100) {
+        echo "Getal tussen de 1 - 100!";
+        exit;
+    } elseif ($invoer < $raadGetal) {
         echo "Hoger!" . PHP_EOL;
     } elseif ($invoer > $raadGetal) {
         echo "Lager!" . PHP_EOL;
